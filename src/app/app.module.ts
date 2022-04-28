1eb43172ae7c97e87e00eb9eb2f1cd4f57875bf2
MileneSeles <milene01seles@gmail.com>
2022-04-28 15:15:29 -0300

    quarto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { FooterComponent } from './components/footer/footer.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import {MatToolbarModule} from 'node_modules/@angular/material/toolbar';
 import { AboutComponent } from './components/about/about.component';
+import {MatCardModule} from '@angular/material/card';
+import { HobbiesComponent } from './components/hobbies/hobbies.component';
+// import {MatGridListModule} from '@angular/material/grid-list';
+// import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
@@ -19,13 +23,17 @@ import { AboutComponent } from './components/about/about.component';
     ContentsComponent,
     FooterComponent,
     GalleryComponent,
-    AboutComponent
+    AboutComponent,
+    HobbiesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    // MatMenuModule
+    // MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
