5014f48405b752659a365b8239aed809d367ee26
MileneSeles <milene01seles@gmail.com>
2022-04-28 11:48:55 -0300

    terceiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ContentsComponent } from './components/contents/contents.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import {MatToolbarModule} from 'node_modules/@angular/material/toolbar';
+import { AboutComponent } from './components/about/about.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import {MatToolbarModule} from 'node_modules/@angular/material/toolbar';
     HeaderComponent,
     ContentsComponent,
     FooterComponent,
-    GalleryComponent
+    GalleryComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
