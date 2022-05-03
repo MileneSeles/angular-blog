1315012a59521c57c0309f808aac6a13a6451368
MileneSeles <milene01seles@gmail.com>
2022-05-03 13:59:38 -0300

    sexto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContentsComponent } from './components/contents/contents.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HobbiesComponent } from './components/hobbies/hobbies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ContentsComponent},
+  {path:'about', component:AboutComponent},
+  {path:'gallery', component: GalleryComponent},
+  {path:'hobbies', component: HobbiesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
